cc3bec8340271d605a6f4129636c5233b5d9dffb
Tom Wright <tom@inflatablecookie.com>
2013-12-06 16:55:26 +0000

    Moved default action to shared area
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/shared/_actions/HttpDefault.php
+++ directory/shared/_actions/HttpDefault.php
@@ -3,7 +3,7 @@
  * This file is part of the Decode Framework
  * @license http://opensource.org/licenses/MIT
  */
-namespace df\apex\directory\front\_actions;
+namespace df\apex\directory\shared\_actions;
 
 use df;
 use df\core;
