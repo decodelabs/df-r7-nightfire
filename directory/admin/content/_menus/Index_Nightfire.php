c14db283f1b85ddd1e6fdc2a405f9af02f90adce
Tom Wright <tom@inflatablecookie.com>
2014-05-21 16:45:29 +0100

    Moved elements to nightfireCore
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- directory/admin/content/_menus/Index_Nightfire.php
+++ directory/admin/content/_menus/Index_Nightfire.php
@@ -18,13 +18,7 @@ class Index_Nightfire extends arch\navigation\menu\Base {
                 ->setId('nodes')
                 ->setDescription('Create full content pages, redirects and more, update versions and control access')
                 ->setIcon('node')
-                ->setWeight(10),
-
-            $entryList->newLink('~admin/content/elements/', 'Elements')
-                ->setId('elements')
-                ->setDescription('Put together reusable blocks of content to use in your pages')
-                ->setIcon('element')
-                ->setWeight(20)
+                ->setWeight(10)
         );
     }
 }
--- directory/admin/content/elements/_templates/Index.html.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo $this->import->component('IndexHeaderBar', '~admin/content/elements/');
-
-echo $this->import->component('ElementList', '~admin/content/elements/');
