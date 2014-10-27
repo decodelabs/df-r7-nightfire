91fd82dbb7a9b0ed727fe820e3d2ab0cbb97fe04
Tom Wright <tom@inflatablecookie.com>
2014-10-27 19:02:25 +0000

    Updated uri output handling
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- libraries/fire/type/Redirect.php
+++ libraries/fire/type/Redirect.php
@@ -14,11 +14,11 @@ use df\aura;
 class Redirect extends Base {
 
     public function createResponse(arch\IContext $context, INode $node, $versionId=null) {
-        return $context->http->redirect($context->normalizeOutputUrl($node['typeData']));
+        return $context->http->redirect($context->uri->__invoke($node['typeData']));
     }
 
     public function renderPreview(aura\view\IView $view, INode $node, $version=null) {
-        return $view->html->link($view->uri->to($node['typeData']))
+        return $view->html->link($view->uri->__invoke($node['typeData']))
             ->setIcon('link')
             ->setTarget('_blank');
     }
